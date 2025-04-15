c8068d8fb02292440c27b80552e0e88d08bce632
nicolenarvaezm <nicolenar94@gmail.com>
2025-04-15 14:14:27 -0500

    implementacion del algoritmo por profundidad

--- src/Models/AlgorithmsModels.ts
+++ src/Models/AlgorithmsModels.ts
@@ -1,5 +1,6 @@
 export const GRID_SIZE = 10;
 
+//posicion
 export interface Position {
     x: number;
     y: number;
@@ -8,9 +9,9 @@ export interface Position {
 // Nodo con estado y camino acumulado.
 export interface QueueNode {
     pos: Position;
-    path: Position[];
-    packagesCollected: boolean[][];
-    collectedCount: number;
+    path: Position[]; //arreglo con el camino recorrido
+    packagesCollected: boolean[][]; //para saber si recogio los paquetes
+    collectedCount: number; //contador con el numero total de paquetes recogidos
   }
 
 // Se expanden en las 4 direcciones: derecha, izquierda, arriba y abajo.
